91c285280166833331f0e2a01e3932e5730f97d0
angel-espinosada <angel.espinosa@udea.edu.co>
2025-09-24 22:18:29 -0500

    Implementando funciones RLE

--- Desafio_1/main.cpp
+++ Desafio_1/main.cpp
@@ -2,6 +2,8 @@
 
 using namespace std;
 
+void convertir_num_atexto(int numero, char resultado[]);
+void compresion_rle(char texto[], char resultado[] );
 int main()
 {
     cout << "Bienvenidos al Desafio 1" << endl;
@@ -31,7 +33,12 @@ int main()
         break;
 
         case 3:{
-
+            char texto[20];
+            char resultado[100];
+            cout<<"Ingrese el texto de prueba"<<endl;
+            cin>>texto;
+            compresion_rle(texto,resultado);
+            cout<<texto<<"Codificado"<<resultado<<endl;
 
         }
         break;
@@ -47,4 +54,61 @@ int main()
      system("pause");
 
     return 0;
+}
+
+
+void convertir_num_atexto(int numero, char resultado[]){
+    int i = 0;
+    int temporal = numero;
+
+    // convierte la cadena a texto
+    for(; temporal > 0; temporal /= 10) {
+        int digito = temporal % 10;
+        resultado[i] = digito + '0';
+        i++;
+    }
+    resultado[i] = '\0';
+
+    // El texto sale invertido
+    int inicio = 0;
+    int fin = i - 1;
+    while(inicio < fin) {
+        char temp = resultado[inicio];
+        resultado[inicio] = resultado[fin];
+        resultado[fin] = temp;
+        inicio++;
+        fin--;
+    }
+}
+
+void compresion_rle(char texto[], char resultado[] ){
+    int i=0;
+    int contado_resul=0;
+
+    while(texto[i]!='\0'){
+        char actual_pos=texto[i];
+        int contador =1;
+        while(texto[i+1]!='\0'&&texto[i+1]==actual_pos){
+            contador=contador +1;
+            i=i+1;
+
+        }
+       //Convierto el resultado a texto para concatenar
+        char numero_texto[10];
+        convertir_num_atexto(contador,numero_texto);
+        int j=0;
+        while(numero_texto[j]!='\0'){
+
+            resultado[contado_resul]=numero_texto[j];
+            contado_resul=contado_resul+1;
+            j=j+1;
+
+        }
+        resultado[contado_resul]=actual_pos;
+        contado_resul=contado_resul+1;
+
+        i=i+1;
+
+    }
+    resultado[contado_resul]='\0';
 }
