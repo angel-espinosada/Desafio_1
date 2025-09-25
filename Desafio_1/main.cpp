9594b8b6c9a3952f5a6a71f7699797ff24c43151
angel-espinosada <angel.espinosa@udea.edu.co>
2025-09-24 23:37:24 -0500

    Se logro hacer compresion y descompresion RLE

--- Desafio_1/main.cpp
+++ Desafio_1/main.cpp
@@ -3,7 +3,8 @@
 using namespace std;
 
 void convertir_num_atexto(int numero, char resultado[]);
-void compresion_rle(char texto[], char resultado[] );
+void compresion_rle(char texto[], char *&resultado, int &tamano );
+void descompresion_rle(char *resultado, int tamano, char *&texto);
 int main()
 {
     cout << "Bienvenidos al Desafio 1" << endl;
@@ -32,16 +33,53 @@ int main()
         }
         break;
 
-        case 3:{
-            char texto[20];
-            char resultado[100];
-            cout<<"Ingrese el texto de prueba"<<endl;
-            cin>>texto;
-            compresion_rle(texto,resultado);
-            cout<<texto<<"Codificado"<<resultado<<endl;
+        case 3: {
+            char texto[100];
+            char *resultado = nullptr;
+            int tamano = 0;
 
+            cout << "Ingrese el texto de prueba: " << endl;
+            cin >> texto;
+
+            compresion_rle(texto, resultado, tamano);
+
+            cout << "Texto original: " << texto << endl;
+            cout << "Codificado (en bytes): ";
+
+            for (int i = 0; i < tamano; i++) {
+                printf("%02X ", (unsigned char)resultado[i]);
+            }
+            cout << endl;
+
+            delete[] resultado;
         }
         break;
+            case 4: {
+                char texto[100];
+                char *resultado = nullptr;
+                char *recuperado = nullptr;
+                int tamano = 0;
+
+                cout << "Ingrese el texto: ";
+                cin >> texto;
+
+                compresion_rle(texto, resultado, tamano);
+
+                cout << "Codificado: ";
+                for (int i = 0; i < tamano; i++) {
+                    printf("%02X ", (unsigned char)resultado[i]);
+                }
+                cout << endl;
+
+                descompresion_rle(resultado, tamano, recuperado);
+
+                cout << "Texto recuperado: " << recuperado << endl;
+
+                delete[] resultado;
+                delete[] recuperado;
+            }
+            break;
+
         default:{
 
             if(opcion!=0)
@@ -81,34 +119,66 @@ void convertir_num_atexto(int numero, char resultado[]){
     }
 }
 
-void compresion_rle(char texto[], char resultado[] ){
+void compresion_rle(char texto[], char *&resultado, int &tamano ){
     int i=0;
     int contado_resul=0;
+
+
+    int longitud=0;
+    while(texto[longitud]!='\0'){
+        longitud++;
+    }
+
+
+    resultado = new char[longitud*3 + 1];
 
     while(texto[i]!='\0'){
         char actual_pos=texto[i];
         int contador =1;
-        while(texto[i+1]!='\0'&&texto[i+1]==actual_pos){
+
+        while(texto[i+1]!='\0' && texto[i+1]==actual_pos){
             contador=contador +1;
             i=i+1;
-
         }
-       //Convierto el resultado a texto para concatenar
-        char numero_texto[10];
-        convertir_num_atexto(contador,numero_texto);
-        int j=0;
-        while(numero_texto[j]!='\0'){
 
-            resultado[contado_resul]=numero_texto[j];
-            contado_resul=contado_resul+1;
-            j=j+1;
 
-        }
-        resultado[contado_resul]=actual_pos;
-        contado_resul=contado_resul+1;
+        resultado[contado_resul] = (contador >> 8) & 0xFF;
+        resultado[contado_resul+1] = contador & 0xFF;
 
+
+        resultado[contado_resul+2] = actual_pos;
+
+
+        contado_resul = contado_resul + 3;
         i=i+1;
-
     }
-    resultado[contado_resul]='\0';
+
+
+
+    tamano = contado_resul;
+}
+
+void descompresion_rle(char *resultado, int tamano, char *&texto) {
+    int contado_texto = 0;
+
+
+
+    texto = new char[1000];
+
+    for (int i = 0; i < tamano; i += 3) {
+        unsigned char byte_alto = resultado[i];
+        unsigned char byte_bajo = resultado[i+1];
+        char caracter = resultado[i+2];
+
+        int contador = (byte_alto << 8) | byte_bajo;
+
+
+        for (int j = 0; j < contador; j++) {
+            texto[contado_texto] = caracter;
+            contado_texto++;
+        }
+    }
+
+
+    texto[contado_texto] = '\0';
 }
